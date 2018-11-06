748952c614f173fdd5bcf1f97c39013571c74877
Bastiaan <mail@bastiaanbiester.nl>
2018-11-06 19:32:12 +0100

    winkelwagen leeg = melding, sessie heet nu 'cart'

--- cart/test.php
+++ cart/test.php
@@ -1,5 +1,6 @@
 <?php
-//test sessie
-$_SESSION['_' . 43] = 2;
-$_SESSION['_' . 23] = 20;
+session_start();
+
+//test sessie met 5 verschillende items
+$_SESSION['cart'] = array ('_43' => 2, '_23' => 1, '_13' => 10, '_3' => 5, '_123' => 1, );
 print_r($_SESSION);
