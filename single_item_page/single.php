9c44d51bc5630347d6aaffbf3d8b3e9c473c7ba5
swilms8835 <swilms8835@student.landstede.nl>
2018-11-06 14:36:44 +0100

    added image to single product page
    Door susan lol

--- single_item_page/single.php
+++ single_item_page/single.php
@@ -3,4 +3,14 @@ require_once ('../connection.php');
 require_once ('../header.php');
 ?>
 
-<
+<div class="container col-md-12 pt-8">
+
+    <div class="img col-md-6">
+        <img src="../images/top_placeholder.png" alt="afbeelding" class="col-md-6">
+    </div>
+
+    <div class="info col-md-6">
+
+    </div>
+
+</div>
