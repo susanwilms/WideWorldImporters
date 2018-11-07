b8b047c3297c560abeea18e6049a6c6d86b104ec
jelle <jellekingma01@gmail.com>
2018-11-07 19:16:54 +0100

    Images

--- Categories.php
+++ Categories.php
@@ -110,7 +110,7 @@ $array = array($driester, $vierster, $vijfster);
 
 <div id="main_container">
     <div class="container pt-4">
-        <img id="img_productgroup" src="/WideWorldImporters/images/productgroup1.jpg">
+        <img id="img_productgroup" src="/WideWorldImporters/images/productgroup<?php print($productgroup)?>.jpg">
 
 
         <div id="test">
