e45c19239672a5d7f1a3f18d1504d7ae3175e302
swilms8835 <swilms8835@student.landstede.nl>
2018-11-07 15:21:21 +0100

    alles uit database
    in single page

--- single.php
+++ single.php
@@ -1,6 +1,30 @@
 <?php
-require_once('connection.php');
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+$productid = $_GET['ProductID'];
+
 require_once('header.php');
+include ('connection.php');
+
+$select = $conn->prepare("SELECT * FROM stockitems WHERE StockItemID = " . $productid);
+
+$select->execute();
+$result = $select->fetchAll();
+
+$productName =      $result[0]["StockItemName"];
+$productPrice =     $result[0]["RecommendedRetailPrice"];
+$productTime =      $result[0]["LeadTimeDays"];
+$productInStock =   2;
+$productColorID =   $result[0]["ColorID"];
+
+$color = $conn->prepare("SELECT * FROM colors WHERE ColorID = 1");
+
+$color->execute();
+$colorresult = $color->fetchAll();
+
+$colorName =        $colorresult[0]["ColorName"];
+
 ?>
 
 <div class="container col-md-12 pt-8" st>
@@ -8,11 +32,16 @@ require_once('header.php');
     <div class="img col-md-6 left" id="product_img">
     </div>
 
-    <div class="info col-md-6 pt-5 right" id="productinfo">
+    <div class="info col-md-6 pt-4 right" id="productinfo">
         <form action="cart.php" method="post">
-            <p>Productnaam</p>
-            <span class="dot" style="background-color: red;"></span><span>Kleur</span><br>
-            <label class="label-aantal">Aantal:</label><input class="number" type="number" name="Aantal" placeholder="1"><br>
+            <input type="hidden" value="<?php print($productid); ?>">
+
+            <p><?php print($productName); ?></p>
+            <p>Prijs: €<?php print($productPrice); ?></p>
+            <p>Verwachte levertijd: <?php print($productTime); ?></p>
+            <p><?php print($productInStock); ?> items op voorraad</p>
+            <span class="dot" style="background-color: <?php echo $colorName; ?>;"></span><span>Kleur</span><br>
+            <label class="label-aantal">Aantal:</label><input class="number" type="number" name="Aantal" placeholder="1">
             <input class="submitbutton" type="submit" name="verzenden" value="Voeg toe">
         </form>
 
@@ -29,22 +58,22 @@ require_once('header.php');
 
                                 <div class="carousel-item active">
                                     <div class="row">
-                                        <div class="col-md-3">
+                                        <div class="col-md-3 carouselitem">
                                             <a href="#">
                                                 <img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
                                             </a>
                                         </div>
-                                        <div class="col-md-3">
+                                        <div class="col-md-3 carouselitem">
                                             <a href="#">
                                                 <img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
                                             </a>
                                         </div>
-                                        <div class="col-md-3">
+                                        <div class="col-md-3 carouselitem">
                                             <a href="#">
                                                 <img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
                                             </a>
                                         </div>
-                                        <div class="col-md-3">
+                                        <div class="col-md-3 carouselitem">
                                             <a href="#">
                                                 <img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
                                             </a>
@@ -56,22 +85,22 @@ require_once('header.php');
 
                                 <div class="carousel-item">
                                     <div class="row">
-                                        <div class="col-md-3">
+                                        <div class="col-md-3 carouselitem">
                                             <a href="#">
                                                 <img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
                                             </a>
                                         </div>
-                                        <div class="col-md-3">
+                                        <div class="col-md-3 carouselitem">
                                             <a href="#">
                                                 <img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
                                             </a>
                                         </div>
-                                        <div class="col-md-3">
+                                        <div class="col-md-3 carouselitem">
                                             <a href="#">
                                                 <img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
                                             </a>
                                         </div>
-                                        <div class="col-md-3">
+                                        <div class="col-md-3 carouselitem">
                                             <a href="#">
                                                 <img src="http://placehold.it/250x250" alt="Image" style="max-width:100%;">
                                             </a>
