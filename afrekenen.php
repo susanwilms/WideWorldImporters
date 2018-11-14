3a7310752c244340082db07e8e2085c843c7d942
DancingTaco <dijk2000@gmail.com>
2018-11-14 11:24:55 +0100

    bier

--- afrekenen.php
+++ afrekenen.php
@@ -1,5 +1,10 @@
 <html>
 <body>
-<img src="iDEAL_ss" alt="Smiley face" height="42" width="42">
+<div class="container pt-5 col-md-1">
+    <div class="row py-2">
+        <div class="col-md-1">
+<img src="./images/iDEAL_ss.png" alt="iDEALpage" height="599" width="480">
+            </div>
+    </div>
 </body>
 </html>
