2f55be0266ec2a6e4f8741d05123225d8b1f3fc4
DancingTaco <dijk2000@gmail.com>
2018-11-14 12:31:57 +0100

    cf

--- afrekenen.php
+++ afrekenen.php
@@ -1,10 +1,11 @@
 <html>
 <body>
-<div class="container pt-5 col-md-1">
-    <div class="row py-2">
-        <div class="col-md-1">
-<img src="./images/iDEAL_ss.png" alt="iDEALpage" height="599" width="480">
-            </div>
+<div>
+    element.style {
+    width: 100%;
+    text-align: center;
+<img src="./images/iDEAL_ss.png" alt="iDEALpage" align="middle" height="599" width="480">
+
     </div>
 </body>
 </html>
