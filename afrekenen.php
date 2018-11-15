09a9ac7fe3bf6db2c0d4b3369d9864aaa421ab36
DancingTaco <dijk2000@gmail.com>
2018-11-15 14:03:24 +0100

    Update afrekenen.php

--- afrekenen.php
+++ afrekenen.php
@@ -1,8 +1,8 @@
 <html>
-<body>
+<body style="background-color:#eef0f0;">
 <div style="width: 100%; text-align: center;">
 
 <img src="./images/afrekenen.png" alt="iDEALpage" align="middle" height="700" width="900"/>
     </div>
-</body>
+</>
 </html>
