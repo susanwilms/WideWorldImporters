ef336771714b5e96c219cee451978cfbe9a43e2b
srzsantiago <s1131631@student.windesheim.nl>
2018-11-18 16:45:21 +0100

    Afrekenen page almost done

--- pay.php
+++ pay.php
@@ -9,7 +9,7 @@ $ingelogd= FALSE;
     .outer {
         display: table;
         position: absolute;
-        height: 100%;
+        height: 80%;
         width: 100%;
     }
 
@@ -21,8 +21,8 @@ $ingelogd= FALSE;
     .inner {
         margin-left: auto;
         margin-right: auto;
-        width: 800px;
-        height: 400px;
+        width: 80%;
+        height: 100%;
         box-shadow: 3px 4px 6px rgba(0, 0, 0,0.5);
         background-color: #ebebeb;
     }
@@ -30,23 +30,24 @@ $ingelogd= FALSE;
     .inner #cellen{
         /*background-color: red;*/
         margin-right: auto;
-        width: 400px;
-        height: 400px;
-
+        width: 65%;
+        height: 100%;
+        padding: 2%;
+        padding-top: 1%;
 
     }
     .inner #cellen:first-child{
         /*background-color: blue;*/
         margin-right: auto;
-        width: 400px;
-        height: 400px;
+        width: 35%;
+        /*height: 400px;*/
         float: right;
         border-left: 1px solid grey;
 
 
     }
     #cellen h2{
-        padding-top: 5%;
+        /*padding-top: 5%;*/
         text-align: center;
     }
     #cellen button{
@@ -55,7 +56,13 @@ $ingelogd= FALSE;
         margin: -40px -100px;
         position:relative;
         left:50%;
-        top: 20%;
+        margin-top: 20%;
+    }
+    #Gast_formulier .form-group{
+        margin-bottom:0rem;
+    }
+    #submit{
+
     }
 </style>
 
@@ -83,12 +90,73 @@ $ingelogd= FALSE;
             </div>
             <div id="cellen">
                 <h2>Afrekenen als gast</h2>
-                <p></p>
-
-                <form >
 
 
-                </form>
+                <div class="container">
+                    <table class="table table-striped">
+
+                                <form class="well form-horizontal">
+                                    <fieldset id="Gast_formulier">
+                                        <div class="form-group">
+                                            <label class="col-md-4 control-label">Volledige Naam</label>
+                                            <div class="col-md-8 inputGroupContainer">
+                                                <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span><input id="fullName" name="fullName" placeholder="Voornaam Achternaam" class="form-control" required="true" value="" type="text"></div>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <label class="col-md-4 control-label">Adres</label>
+                                            <div class="form-inline">
+                                                <div class="col-md-4 inputGroupContainer">
+                                                    <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="addressLine1" name="addressLine1" placeholder="Straat Huisnummer" class="form-control" required="true" value="" type="text"></div>
+                                                </div>
+                                                <div class="col-md-4 inputGroupContainer">
+                                                    <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="postcode" name="postcode" placeholder="Postal Code/ZIP" class="form-control" required="true" value="" type="text"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="col-md-4 control-label">Stad</label>
+                                            <div class="col-md-8 inputGroupContainer">
+                                                <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span><input id="city" name="city" placeholder="Stad" class="form-control" required="true" value="" type="text"></div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="col-md-4 control-label">Land</label>
+                                            <div class="col-md-8 inputGroupContainer">
+                                                <div class="input-group">
+                                                    <span class="input-group-addon" style="max-width: 100%;"><i class="glyphicon glyphicon-list"></i></span>
+                                                    <select class="selectpicker form-control">
+                                                        <option>Nederland</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="col-md-4 control-label">Email</label>
+                                            <div class="col-md-8 inputGroupContainer">
+                                                <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span><input id="email" name="email" placeholder="Email" class="form-control" required="true" value="" type="text"></div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label class="col-md-4 control-label">Phone Number</label>
+                                            <div class="col-md-8 inputGroupContainer">
+                                                <div class="input-group"><span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span><input id="phoneNumber" name="phoneNumber" placeholder="Phone Number" class="form-control" required="true" value="" type="text"></div>
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div class="col-md-4 control-label">
+                                                <button type="submit" class="btn btn-default">Submit</button>
+                                            </div>
+                                        </div>
+
+                                    </fieldset>
+                                </form>
+                    </table>
+
+
+                </div>
+
             </div>
 
         </div>
