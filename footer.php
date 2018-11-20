c7f9025336f94ab18e6aeb42c0f182afcbd23b00
swilms8835 <swilms8835@student.landstede.nl>
2018-11-20 15:29:21 +0100

    Center items in footer

--- footer.php
+++ footer.php
@@ -1,8 +1,10 @@
 
 </body>
 <footer>
-    <a href="/about.php">Over ons</a> |
-    <a href="/contact.php">Contact</a> |
-    <a href="/terms.php">Algemene voorwaarden</a>
+    <div class="footer_content">
+        <a href="/about.php">Over ons</a> |
+        <a href="/contact.php">Contact</a> |
+        <a href="/terms.php">Algemene voorwaarden</a>
+    </div>
 
 </footer>
