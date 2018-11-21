76d4d042093adad78a3507204cf1e79e5f4f5d2b
bastiaanbiester <mail@bastiaanbiester.nl>
2018-11-21 13:15:33 +0100

    fix 2

--- footer.php
+++ footer.php
@@ -2,9 +2,9 @@
 </body>
 <footer>
     <div class="footer_content">
-        <a href="WideWorldImporters/about.php">Over ons</a> |
-        <a href="WideWorldImporters/contact.php">Contact</a> |
-        <a href="WideWorldImporters/terms.php">Algemene voorwaarden</a>
+        <a href="/WideWorldImporters/about.php">Over ons</a> |
+        <a href="/WideWorldImporters/contact.php">Contact</a> |
+        <a href="/WideWorldImporters/terms.php">Algemene voorwaarden</a>
     </div>
 
 </footer>
