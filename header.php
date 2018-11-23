0178df9d5f340b418d2e669d2c33c06bf3d218dc
swilms8835 <swilms8835@student.landstede.nl>
2018-11-23 12:28:32 +0100

    navbar fixed

--- header.php
+++ header.php
@@ -20,7 +20,7 @@ $productgroup="";
     <script src="./includes/bootstrap/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="style.css">
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
         <a class="navbar-brand" href="/WideWorldImporters/index.php"><img class="logo" src="images/logo.png"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
