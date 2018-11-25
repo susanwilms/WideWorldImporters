f125718de98808a4fd1678ff9fcbdee7e64299ab
bastiaanbiester <mail@bastiaanbiester.nl>
2018-11-25 22:03:35 +0100

    simplified index.php

--- index.php
+++ index.php
@@ -1,26 +1,8 @@
 <?php
+
 require_once 'connection.php';
 require_once 'header.php';
 
-// all variables used in this document
-$productgroup = (int)filter_input(INPUT_GET, "productgroup", FILTER_SANITIZE_STRING);
-$sort = (int)filter_input(INPUT_GET, "sort", FILTER_SANITIZE_STRING);
-$limit = (int) filter_input(INPUT_GET, "limit", FILTER_SANITIZE_STRING);
-$page = (int) filter_input(INPUT_GET, "page", FILTER_SANITIZE_STRING);
-
-$generalURL = "/WideWorldImporters/Categories.php?productgroup=". $productgroup;
-
-// array with all possible sort options
-$sort_options = array (0 => "sisg.StockItemID ASC", 1 => "RecommendedRetailPrice ASC", 2 => "RecommendedRetailPrice DESC");
-$sorted = $sort_options[$sort];
-
-// QUERY 1, for getting the itemID, Name, and price.
-$stmtcat = $conn->prepare("SELECT sisg.StockItemID, si.StockItemName, si.RecommendedRetailPrice FROM stockitemstockgroups sisg JOIN stockitems si ON sisg.StockItemID=si.StockItemID WHERE StockGroupID = :groupid ORDER BY ${sorted} LIMIT :page,:limit;");
-$stmtcat->bindParam(':groupid', $productgroup);
-$stmtcat->bindParam(':limit', $limit, PDO::PARAM_INT);
-$stmtcat->bindParam(':page', $pages, PDO::PARAM_INT);
-$stmtcat->execute();
-$resultcat1 = $stmtcat->fetchAll();
 
 // generation of random review stars
 $threestar = "<span>★</span><span>★</span><span>★</span><span>☆</span><span>☆</span>";
@@ -43,21 +25,21 @@ $array = array($threestar, $fourstar, $fivestar);
         $item4 = rand(1, 227);
     } while ($item1 == $item4 or $item2 == $item4 or $item3 == $item4);
 
-    $stmtcat2 = $conn->prepare("SELECT StockItemID, StockItemName, RecommendedRetailPrice FROM stockitems WHERE StockItemID = :item1 or StockItemID = :item2 or StockItemID = :item3 or StockItemID = :item4");
-    $stmtcat2->bindParam(':item1', $item1);
-    $stmtcat2->bindParam(':item2', $item2);
-    $stmtcat2->bindParam(':item3', $item3);
-    $stmtcat2->bindParam(':item4', $item4);
-    $stmtcat2->execute();
-    $resultcat2 = $stmtcat2->fetchAll();
+    $stmt = $conn->prepare("SELECT StockItemID, StockItemName, RecommendedRetailPrice FROM stockitems WHERE StockItemID in (:item1, :item2, :item3, :item4)");
+    $stmt->bindParam(':item1', $item1);
+    $stmt->bindParam(':item2', $item2);
+    $stmt->bindParam(':item3', $item3);
+    $stmt->bindParam(':item4', $item4);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
     ?>
 
 </p>
 <div class="container">
     <div class="row" id="Categories">
         <?php
-        if(!empty($resultcat2)){
-            foreach($resultcat2 as $r){
+        if(!empty($result)){
+            foreach($result as $r){
                 $stock_id = $r[0];
                 $stock_name = $r[1];
                 $stock_price = $r[2];
@@ -78,7 +60,7 @@ $array = array($threestar, $fourstar, $fivestar);
             ?>
     </div>
 </div>
+
 <?php
     require_once 'footer.php';
 ?>
-</body>
