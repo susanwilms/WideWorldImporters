6f772bd03dcce6ba7e414cbaeea867ad162741d0
Bastiaan <mail@bastiaanbiester.nl>
2018-11-30 13:35:22 +0100

    added comments

--- installatie/add_image.php
+++ installatie/add_image.php
@@ -11,6 +11,7 @@ echo("img_path tabel succesvol geleegd<br>");
 $path    = "../images/";
 $files = scandir($path);
 // excluding non product images
+// make sure every non-product image in the /images/ folder is in this array.
 $files = array_diff(scandir($path), array(".", "..", "cart.png", "cart_icon.png", "favicon-32x32.png", "favicon-16x16.png", "favicon.ico", "header.jpg", "PicProduct1.png", "pay.png", "logo.png", "placeholder.png", "productgroup1.jpg", "productgroup2.jpg", "productgroup3.jpg", "productgroup4.jpg", "productgroup5.jpg", "productgroup6.jpg", "productgroup7.jpg", "productgroup8.jpg", "productgroup9.jpg", "productgroup10.jpg", "top_placeholder.png"));
 
 // loop through each file and add it to the db
